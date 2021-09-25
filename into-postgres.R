a6169a8adc0316a3f2cda684fdee8dad70e7d721
Zhen3r <xzzxzz2010@163.com>
2021-09-25 00:05:23 -0400

    add invalid letter check

--- into-postgres.R
+++ into-postgres.R
@@ -6,6 +6,7 @@ library(tidyverse)
 library(RPostgres)
 # setwd("...")
 
+# settings
 db.name = "..."
 table.name = "..."
 host = "localhost"
@@ -14,16 +15,27 @@ username = "postgres"
 password = "..."
 file.name = "./example.csv"
 
+# publish connection
 conn <- dbConnect(Postgres(), dbname = dbname, host = host, port = port, 
                   user = username, password = password)
 
+# read file
 if (file.name%>%endsWith(".csv")){
   s = read_csv(file.name)
 }else{
   s = st_read(file.name)
 }
 
-glimpse(s)
+# check if there are invalid characters
 s = s%>%rename_all(tolower)
-st_write(obj = s, dsn = conn, Id(table = table.name))
+check.invalid = function(t){
+  return(!str_match(t,"[^\\w]")[1,1] %>% is.na())
+}
+if (check.invalid(s%>%colnames())){
+  stop("Invalid character found! Please check column names.")
+}
 
+glimpse(s)
+
+# writing to postgres
+st_write(obj = s, dsn = conn, Id(table = table.name))
